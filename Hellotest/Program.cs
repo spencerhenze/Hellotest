a625450d50d035b316f75a99723230f63b659fc5
Spencer Henze <spencer.henze@allata.com>
2018-09-24 19:33:22 -0600

    Add project files.

---
+++ Hellotest/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Hellotest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
